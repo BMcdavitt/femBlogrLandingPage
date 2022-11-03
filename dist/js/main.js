62162d02d9eac1ad54dc9ea71402d0a9fd062909
BMcDavitt <bill.mcdavitt@yahoo.com>
2022-11-03 19:34:15 -0400

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,73 @@
+const menu = document.querySelector('.menu')
+const hamburger = document.querySelector('.hamMiddle')
+const hamBefore = document.querySelector('.hamBefore')
+const hamAfter = document.querySelector('.hamAfter')
+const hamburgerArea = document.querySelector('.hamburgerArea')
+
+const product = document.getElementById('menuCategory_Product')
+const productItems = document.getElementById('items_product')
+const productArrow = document.getElementById('dropdown_Product')
+const company = document.getElementById('menuCategory_Company')
+const companyItems = document.getElementById('items_company')
+const companyArrow = document.getElementById('dropdown_Company')
+const connect = document.getElementById('menuCategory_Connect')
+const connectItems = document.getElementById('items_connect')
+const connectArrow = document.getElementById('dropdown_Connect')
+
+let menuVisible = false
+let sectionExpanded = 'none'
+let sectionArrowExpanded = 'none'
+
+hamburger.addEventListener('click', toggleMenu)
+hamBefore.addEventListener('click', toggleMenu)
+hamAfter.addEventListener('click', toggleMenu)
+hamburgerArea.addEventListener('click', toggleMenu)
+
+product.addEventListener('click', () => {
+  expandMenu(productItems, productArrow)
+})
+company.addEventListener('click', () => {
+  expandMenu(companyItems, companyArrow)
+})
+connect.addEventListener('click', () => {
+  expandMenu(connectItems, connectArrow)
+})
+
+function toggleMenu() {
+  if (menuVisible === false) {
+    menuVisible = true
+    menu.style.animation = 'slideDown 1s forwards'
+    hamburger.style.animation = 'fadeout 0.5s forwards'
+    hamBefore.style.animation = 'slantDown 0.5s forwards'
+    hamAfter.style.animation = 'slantUp 0.5s forwards'
+  } else {
+    menuVisible = false
+    menu.style.animation = 'slideUp 1s forwards'
+    hamburger.style.animation = 'fadein 0.5s forwards'
+    hamBefore.style.animation = 'slantDownBack 0.5s forwards'
+    hamAfter.style.animation = 'slantUpBack 0.5s forwards'
+    if (sectionExpanded !== 'none') {
+      sectionExpanded.style.animation = 'collapse 1s forwards'
+      sectionArrowExpanded.style.animation = 'unrotateArrow 1s forwards'
+      sectionExpanded = 'none'
+    }
+  }
+}
+
+function expandMenu(section, sectionArrow) {
+  if (sectionExpanded === section) {
+    section.style.animation = 'collapse 1s forwards'
+    sectionArrow.style.animation = 'unrotateArrow .25s forwards'
+    sectionExpanded = 'none'
+  } else {
+    if (sectionExpanded !== 'none') {
+      sectionExpanded.style.animation = 'collapse 1s forwards'
+      sectionArrowExpanded.style.animation = 'unrotateArrow .25s forwards'
+    }
+
+    sectionExpanded = section
+    sectionArrowExpanded = sectionArrow
+    section.style.animation = 'expand 1s forwards'
+    sectionArrow.style.animation = 'rotateArrow .25s forwards'
+  }
+}
